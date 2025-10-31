34f8ef6ee45c4b30f2b74088ffbdcc42dee1b88d
Tom Mangione <tommangione@pm.me>
2025-10-31 12:57:30 -0500

    functions created but not yet functional

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,28 @@
 use::std::io;
 
 fn main() {
-    println!("Select the function you want.\nEnter 1 for Fibonacci number calculator\nEnter 2 for Celsius to Farenheit\nEnter 3 for Farenheit to Celsius");
-    let mut selection = String::new();
     
+    //prints selection menu
+    println!("Select the function you want.\nEnter 1 for Fibonacci number calculator\nEnter 2 for Celsius to Farenheit\nEnter 3 for Farenheit to Celsius");
+    
+    //initializes an empty string. Only use the empty string literal with an immutable string
+    //  variable
+    let mut selection = String::new();
+
+    //stdin().read_line() grabs a string from the command line
     io::stdin().read_line(&mut selection).expect("Failed to read line.");
     
+    //selection is reassigned to itself, but as type u8
+    //in Rust, we say that the previous value/type is shadowed by the new one
     let selection: u8 = selection.trim().parse().expect("");
-
+    
+    //selection menu logic
     if selection == 1 {
         fib();
-    } else if selection == 2 || selection == 3 {
-        println!("Not yet implemented.");
+    } else if selection == 2 {
+        c_to_f();
+    } else if selection == 3 {
+        f_to_c();
     } else {
         println!("Input not recognized.");
     }
@@ -22,4 +33,12 @@ fn main() {
 
 fn fib() {
     println!("Fib function!");
+}
+
+fn c_to_f() {
+    println!("C to F function!");
+}
+
+fn f_to_c() {
+    println!("F to C function!");
 }
